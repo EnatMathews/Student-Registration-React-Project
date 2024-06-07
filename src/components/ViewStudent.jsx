79318f883e811223dcc0ca06fd168699f3fae086
EnatMathews <enatmathews@gmail.com>
2024-06-07 15:15:08 +0530

    Project created

---
+++ src/components/ViewStudent.jsx
@@ -0,0 +1,122 @@
+import React from 'react'
+import NavbarStudent from './NavbarStudent'
+
+const ViewStudent = () => {
+  return (
+    <div>
+    <NavbarStudent/>
+   <div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+        
+            <div className="row g-3">
+                <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                <div class="card" >
+  <img src="https://altasmiles.com/wp-content/uploads/2019/10/iStock-510857204.jpg" class="card-img-top" alt="..." height="500"/>
+  <div class="card-body">
+    <h5 class="card-title">A</h5>
+    
+    <a href="#" class="btn btn-primary">View More</a>
+  </div>
+</div>
+                </div>
+                <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                <div class="card">
+  <img src="https://purepng.com/public/uploads/large/purepng.com-female-studentstudenteducationalinstitutionstudentsuniversity-studentschoolfemale-student-1421526922477gbjew.png" class="card-img-top" alt="..." height="500"/>
+  <div class="card-body">
+    <h5 class="card-title">B</h5>
+
+    <a href="#" class="btn btn-primary">View More</a>
+  </div>
+</div>
+                </div>
+                <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                <div class="card" >
+  <img src="https://purepng.com/public/uploads/large/purepng.com-female-studentstudenteducationalinstitutionstudentsuniversity-studentschoolfemale-student-1421526922642xwzty.png" class="card-img-top" alt="..." height="500"/>
+  <div class="card-body">
+    <h5 class="card-title">C</h5>
+    
+    <a href="#" class="btn btn-primary">View More</a>
+  </div>
+</div>
+                </div>
+                <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                <div class="card" >
+  <img src="https://www.kindpng.com/picc/m/40-405645_college-student-images-png-transparent-png.png" class="card-img-top" alt="..." height="500"/>
+  <div class="card-body">
+    <h5 class="card-title">D</h5>
+
+    <a href="#" class="btn btn-primary">View More</a>
+  </div>
+</div>
+                </div>
+                <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                <div class="card" >
+  <img src="https://www.pngall.com/wp-content/uploads/8/Student-PNG-Photo.png" class="card-img-top" alt="..." height="500"/>
+  <div class="card-body">
+    <h5 class="card-title">E</h5>
+
+    <a href="#" class="btn btn-primary">View More</a>
+  </div>
+</div>
+                </div>
+                <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                <div class="card" >
+  <img src="https://www.pngall.com/wp-content/uploads/8/Female-Student-PNG.png" class="card-img-top" alt="..." height="500"/>
+  <div class="card-body">
+    <h5 class="card-title">F</h5>
+   
+    <a href="#" class="btn btn-primary">View More</a>
+  </div>
+</div>
+                </div>
+                <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                <div class="card" >
+  <img src="https://www.kindpng.com/picc/m/117-1176758_boy-student-pic-hd-hd-png-download.png" class="card-img-top" alt="..." height="500"/>
+  <div class="card-body">
+    <h5 class="card-title">G</h5>
+    
+    <a href="#" class="btn btn-primary">View More</a>
+  </div>
+</div>
+                </div>
+                <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                <div class="card" >
+  <img src="https://www.avanse.com/viewPagesAssets/img/study-in-india-banner.webp" class="card-img-top" alt="..." height="500"/>
+  <div class="card-body">
+    <h5 class="card-title">H</h5>
+
+    <a href="#" class="btn btn-primary">View More</a>
+  </div>
+</div>
+                </div>
+                <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                <div class="card" >
+  <img src="https://pngimg.com/uploads/student/student_PNG62541.png" class="card-img-top" alt="..." height="500"/>
+  <div class="card-body">
+    <h5 class="card-title">I</h5>
+    
+    <a href="#" class="btn btn-primary">View More</a>
+  </div>
+</div>
+                </div>
+                <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                <div class="card" >
+  <img src="https://purepng.com/public/uploads/large/purepng.com-female-studentstudentcollege-studentschool-studentfemale-student-1421526923009tqoyh.png" class="card-img-top" alt="..." height="500"/>
+  <div class="card-body">
+    <h5 class="card-title">J</h5>
+   
+    <a href="#" class="btn btn-primary">View More</a>
+  </div>
+</div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
+
+  )
+}
+
+export default ViewStudent
